4b4877946d19fe9420255f398946d30228391049
Dario Contreras <dario.contreras@metricstudio.cl>
2025-01-05 19:23:23 -0300

    refactor: add env variables on database config

--- config/database.js
+++ config/database.js
@@ -4,24 +4,25 @@
 
 module.exports = {
   development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "parkingdb_dev",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "parkingdb_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "parkingdb",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    logging: false,
   },
 };
